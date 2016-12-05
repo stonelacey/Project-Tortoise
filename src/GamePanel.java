8fe8bd6a384aeb82d9503c0593c8994894fcfb8d
mjg5683 <mjg5683@202-WD34.up.ist.local>
2016-12-05 15:47:42 -0500

    Trying to draw rectangle on key press

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 public class GamePanel extends JPanel
@@ -13,8 +14,21 @@ public class GamePanel extends JPanel
     private final String MOVE_LEFT = "move left";
     private final String MOVE_RIGHT = "move right";
     
+    //Movements
+    private final int UP = 0;
+    private final int DOWN = 1;
+    private final int RIGHT = 2;
+    private final int LEFT = 3;
+    
+    private final JButton button;
+    
     private OptionsModel optMod;
     private JPanel gameArea;
+    
+    int snake1CurrentX = 78;
+    int snake1CurrentY = 225;
+    int snake2CurrentX = 700;
+    int snake2CurrentY = 225;
     
     GamePanel(OptionsModel optMod)
     {
@@ -23,15 +37,12 @@ public class GamePanel extends JPanel
         gameArea = new JPanel();
         gameArea.setSize(750, 450);
         
-        JLabel backcolor = new JLabel();
-        JLabel snake1color = new JLabel();
-        JLabel snake2color = new JLabel();
-        Object snake1 = new Object();
-        add(backcolor);
-        add(snake1color);
-        add(snake2color);
-        add(gameArea);
+        button = new JButton("Test button, please ignore.");
+        button.setBounds(0,0,1,1);
         
+        add(gameArea);
+        button.setVisible(false);
+        add(button);
         
     }
     
@@ -67,28 +78,66 @@ public class GamePanel extends JPanel
             
         }
         
+        
+        
         //Background color
         g.setColor(optMod.getBackgroundTempColor());
         g.fillRect(53, 53, 698, 397);
         
         //Snake 1 Start
         g.setColor(optMod.getSnake1Color());
-        g.fillRect(78, 225, 25, 25);
+        g.fillRect(snake1CurrentX, snake1CurrentY, 25, 25);
         
         //Snake 2 Start
-        g.setColor(optMod.getSnake2Color());
-        g.fillRect(700, 225, 25, 25);
         
-        snake1color.getInputMap(IFW).put(KeyStroke.getKeyStroke("UP"), MOVE_UP);
-        snake1color.getInputMap(IFW).put(KeyStroke.getKeyStroke("DOWN"), MOVE_DOWN);
-        snake1color.getInputMap(IFW).put(KeyStroke.getKeyStroke("LEFT"), MOVE_LEFT);
-        snake1color.getInputMap(IFW).put(KeyStroke.getKeyStroke("RIGHT"), MOVE_RIGHT);
+        g.setColor(optMod.getSnake2Color());
+        g.fillRect(snake2CurrentX, snake2CurrentY, 25, 25);
+        
+        //Input maps
+        button.getInputMap(IFW).put(KeyStroke.getKeyStroke("UP"), MOVE_UP);
+        button.getInputMap(IFW).put(KeyStroke.getKeyStroke("DOWN"), MOVE_DOWN);
+        button.getInputMap(IFW).put(KeyStroke.getKeyStroke("LEFT"), MOVE_LEFT);
+        button.getInputMap(IFW).put(KeyStroke.getKeyStroke("RIGHT"), MOVE_RIGHT);
+        
+        //Action maps
+        button.getActionMap().put(MOVE_UP, new MoveAction(UP));
+        button.getActionMap().put(MOVE_DOWN, new MoveAction(DOWN));
+        button.getActionMap().put(MOVE_LEFT, new MoveAction(LEFT));
+        button.getActionMap().put(MOVE_RIGHT, new MoveAction(RIGHT));
     
+    }
+    
+    private class MoveAction extends AbstractAction {
+        private final int direction;
+        private final int speed = 5;
+        
+        public MoveAction(int direction) {
+            this.direction = direction;
+        }
+        
+        
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          //  g.setColor(optMod.getSnake2Color());
+            if (direction == UP) {
+                snake2CurrentY += 25;
+            } if (direction == DOWN) {
+                snake2CurrentY -= 25;
+            } if (direction == RIGHT) {
+                snake2CurrentX += 25;
+            } if (direction == LEFT) {
+                snake2CurrentX -= 25;
+            }
+         //   g.fillRect(snake2CurrentX, snake2CurrentY, 25, 25);
+         System.out.println(snake2CurrentX + " " + snake2CurrentY);
+            
+        }
     }
     
     public void setColor(String color3, String color1, String color2)
     {
-        
+       
     }
     
 }
